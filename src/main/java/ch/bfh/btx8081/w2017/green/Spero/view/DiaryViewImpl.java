19f0601f7739220f3ecb395bd68a4831b3ed171b
schmt5 <32765638+schmt5@users.noreply.github.com>
2017-11-29 20:28:32 +0100

    DiaryViewImpl: Hinzufügen von Panel

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/view/DiaryViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/view/DiaryViewImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
 
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.SperoView;
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.SperoViewListener;
@@ -13,7 +16,22 @@ public class DiaryViewImpl extends CustomComponent implements SperoView {
 	
 	private List<SperoViewListener> listeners = new ArrayList<>();
 	
+	private Panel panel;
+	
 	public DiaryViewImpl() {
+		VerticalLayout layout = new VerticalLayout();
+		setCompositionRoot(layout);
+		
+		panel = new Panel("PanelTest");
+		panel.setHeight(100.0f, Unit.PERCENTAGE);
+		
+		VerticalLayout contentLayout = new VerticalLayout();
+		contentLayout.setWidth(500, Unit.PIXELS);
+		contentLayout.setSpacing(false);
+		contentLayout.addComponent(new Label("PanelTestLabel"));
+		
+		panel.setContent(contentLayout);
+		
 		
 	}
 	
