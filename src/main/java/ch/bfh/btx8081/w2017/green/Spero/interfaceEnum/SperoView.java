2e62bc82f2c29cfe935813bc873a115441059cfb
stahm2 <32765598+stahm2@users.noreply.github.com>
2017-12-02 11:17:08 +0100

    backup

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/interfaceEnum/SperoView.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/interfaceEnum/SperoView.java
@@ -1,10 +1,13 @@
 package ch.bfh.btx8081.w2017.green.Spero.interfaceEnum;
 
+import com.vaadin.event.MouseEvents.ClickListener;
 import com.vaadin.navigator.View;
-import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Button.ClickEvent;
 
-public interface SperoView extends View, ClickListener {
-    // XXX: public void setDisplay(double disp);
+public interface SperoView extends View, SperoViewListener {
+//	public void setDisplay(double disp); test
+	
+	public void addListener(SperoViewListener listener);
 
-    public void addListener(ClickListener listener);
+
 }
