ccaf6d9956517742fdf8408ff537665b1bdf75ca
stahm2 <32765598+stahm2@users.noreply.github.com>
2017-12-02 11:17:48 +0100

    backup

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/Diary.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/Diary.java
@@ -6,45 +6,54 @@ import java.util.List;
 import java.util.Map;
 
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.Mood;
-import lombok.Getter;
 
 public class Diary {
-    @Getter
-    private int numberOfDiaryEntries = 0;
-    private final Map<Integer, DiaryEntry> diaryEntryMap;
-
-    public Diary() {
-	this.diaryEntryMap = new HashMap<Integer, DiaryEntry>();
-    }
-
-    public void createEntry(String diaryTitle, String diaryText, Mood moodParam) {
-	final int index = this.numberOfDiaryEntries + 1;
-	final DiaryEntry diaryEntry = new DiaryEntry(index, diaryTitle, diaryText, moodParam);
-	this.diaryEntryMap.put(index, diaryEntry);
-	// increase index after putting into map
-	this.numberOfDiaryEntries++;
-    }
-
-    public void deleteEntry(int diaryEntryNumber) {
-	this.diaryEntryMap.remove(diaryEntryNumber);
-    }
-
-    public DiaryEntry searchEntry(int DiaryEntryNumber) {
-	return this.diaryEntryMap.get(DiaryEntryNumber);
-    }
-
-    public List<DiaryEntry> searchTag() {
-	// TODO: implementation
-	return null;
-    }
-
-    public String printDiaryEntries() {
-	final List<DiaryEntry> list = new ArrayList<DiaryEntry>();
-	list.addAll(this.diaryEntryMap.values());
-	String str = "";
-	for (final DiaryEntry diaryEntry : list) {
-	    str = str + diaryEntry.toString() + "/n";
+	private int numberOfDiaryEntry = 0;
+	private Map<Integer, DiaryEntry> diaryEntryMap;
+	//test
+	
+	public Diary() {
+		diaryEntryMap = new HashMap<>();
 	}
-	return str;
-    }
+
+	public void createEntry(String diaryTitle, String diaryText, Mood moodParam) {
+		
+		
+		this.diaryEntryMap.put(numberOfDiaryEntry + 1, new DiaryEntry(numberOfDiaryEntry + 1, diaryTitle, diaryText, moodParam));
+		this.numberOfDiaryEntry++;
+		
+	}
+
+	public void deleteEntry(int diaryEntryNumber) {
+		
+		
+		this.diaryEntryMap.remove(diaryEntryNumber);
+		
+	}
+
+	public DiaryEntry searchEntry(int DiaryEntryNumber) {
+		
+		return diaryEntryMap.get(DiaryEntryNumber);
+	}
+	
+	public int getNumberOfDiaryEntry() {
+		return numberOfDiaryEntry;
+	}
+
+	public List<DiaryEntry> searchTag() {
+		
+		return null;
+	}
+	
+	public String printDiaryEntry() {
+		List<DiaryEntry> list = new ArrayList<DiaryEntry>();
+		list.addAll(diaryEntryMap.values());
+		String str = "";
+		
+		for(DiaryEntry diaryEntry : list) {
+			str = str + diaryEntry.toString() + " /n";
+		}
+		
+		return str;
+	}
 }
