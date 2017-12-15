d8aefaf7d287a68105b3022099c452cf0e62daec
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2017-12-15 09:53:04 +0100

    chatButton

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/ChangePinModel.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/ChangePinModel.java
@@ -33,88 +33,21 @@ public class ChangePinModel {
     
     public void checkNewPin(String newPin1, String newPin2)
     	throws IncorrectException {
-    	
-//    	try {
-//    		Integer.parseInt(newPin1.substring(0));
-//    	}
-//    	catch (Exception e) {
-//    		System.out.println("nur Zahlen");
-//    		throw new IncorrectException("Zahl");
-//    	}
-    	
-    	//boolean check = onlyNumbers(newPin1);
-    	
     	if (!newPin2.equals(newPin1)) {
     		throw new IncorrectException("Die beiden neuen PINs stimmen nicht überein.");
     	} else if (newPin1.isEmpty()==true){
     		throw new IncorrectException("Der PIN darf nicht leer sein.");
     	} else if (newPin1.length() !=4) {
     		throw new IncorrectException("Der PIN hat mehr oder weniger als 4 Ziffern.");
-    //	} else if (Integer.parseInt(newPin1.substring(0));) {
-    		
-//    	} else if (check=false) {
-//    		throw new IncorrectException("Der PIN darf nur Zahlen enthalten.");
-    	
     	} else {
     		System.out.println("newPin korrekt");
     	}
-    	//switch (newPin1) {
-    	//case():
     	try {
     		Integer.parseInt(newPin1.substring(0));
     	}
     	catch (Exception e) {
     		System.out.println("nur Zahlen");
-    		//changePinView.pinException.setValue("nur Zahlen");
     		throw new IncorrectException("Der PIN darf nur aus Zahlen bestehen.");
-    		//break;
     	}
     	}
     }
-    
-//    public boolean onlyNumbers(String newPin1) {
-//    	
-//    	int firstNumber = Integer.parseInt(newPin1.substring(0,0));
-//    	int secondNumber = Integer.parseInt(newPin1.substring(1,1));
-//    	int thirdNumber = Integer.parseInt(newPin1.substring(2,2));
-//    	int forthNumber = Integer.parseInt(newPin1.substring(3,3));
-//    	
-//    	int first = 0;
-//    	int second = 0;
-//    	int third = 0;
-//    	int forth = 0;
-//    	
-//    	for (int i = 0; i<10; i++) {
-//    		if (firstNumber == i) {
-//    			first = 1;
-//    		}
-//    	}
-//    	
-//    	for (int i = 0; i<10; i++) {
-//    		if (secondNumber == i) {
-//    			second = 1;
-//    		}
-//    	}
-//    	
-//    	for (int i = 0; i<10; i++) {
-//    		if (thirdNumber == i) {
-//    			third = 1;
-//    		}
-//    	}
-//    	
-//    	for (int i = 0; i<10; i++) {
-//    		if (forthNumber == i) {
-//    			forth = 1;
-//    		}
-//    	}
-//    	
-//    	int sum = first + second + third + forth;
-//    	
-//    	if (sum == 4){
-//    		return true;
-//    	} else {
-//    		return false;
-//    	}
-//    }
-
-//}
