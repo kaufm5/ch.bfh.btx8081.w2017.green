6e92607cf60e3e7e43a641fdfbb8963f2583f82c
stahm2 <32765598+stahm2@users.noreply.github.com>
2017-12-22 09:29:07 +0100

    gibt den reminder in der konsole aus

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/ReminderTask.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/ReminderTask.java
@@ -4,11 +4,17 @@ import java.util.TimerTask;
 
 public class ReminderTask extends TimerTask {
 
-		int loop = 5;
+		public String name;
+		private int loop = 5;
+		
+		ReminderTask(String name){
+			this.name = name;
+			run();
+		}
 		 
 		public void run() {
 			if (loop > 0) {
-				System.out.format("Knock Knock..!\n");
+				System.out.println("Einnehmen: " + name);
 				loop--;
 			} else {
 				System.out.format("\nThat's it.. Done..!");
