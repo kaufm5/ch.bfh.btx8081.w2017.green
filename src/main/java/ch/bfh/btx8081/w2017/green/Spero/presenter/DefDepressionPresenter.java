ff4f7dbf219e44c62522b56ce2f5785724689984
mussi <mussi@147.87.243.11>
2018-01-12 08:55:51 +0100

    javadoc for DefDepressionPresenter

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/presenter/DefDepressionPresenter.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/presenter/DefDepressionPresenter.java
@@ -6,30 +6,46 @@ import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.SperoViewListener;
 import ch.bfh.btx8081.w2017.green.Spero.view.DefDepressionView;
 import ch.bfh.btx8081.w2017.green.Spero.view.Views;
 
+/**
+ * This class shows all the necessary definitions and information about depression 
+ * and all the necessary data to the DefDepressionView
+ * 
+ * @author mussi
+ */
 public class DefDepressionPresenter implements SperoViewListener{
-	
-private DefDepressionView view;
-	
+
+	private static final long serialVersionUID = 1L;
+
+	private DefDepressionView view;
+
+	/**
+	 * Class constructor 
+	 * 
+	 * @param view - the view it should present 
+	 */
 	public DefDepressionPresenter(DefDepressionView view) {
 		this.view = view;
 		view.init();
 		view.addListener(this);
 	}
-// test
+
+	/**
+	 * Method for switching a button id to navigate to a new view via URL 
+	 */
 	@Override
 	public void buttonClick(ClickEvent event) {
 		String buttonId = event.getButton().getId();
-		
+
 		switch(buttonId) {
 		case "menuButton":
 			view.getUI().getNavigator().navigateTo(Views.MENU_VIEW);
 		case "sosButton":
 			view.getUI().getNavigator().navigateTo(Views.SOS_VIEW);
-		    break;
+			break;
 		case "chatButton":
 			view.getUI().getNavigator().navigateTo(Views.CHAT_VIEW);
-		    break;
-	}
+			break;
+		}
 	}
 
 }
