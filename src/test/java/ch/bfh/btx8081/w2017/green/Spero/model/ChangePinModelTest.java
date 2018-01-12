d9345a4a689234461e033246b024a26d88150ec3
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2018-01-12 13:19:02 +0100

    junit

--- src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/ChangePinModelTest.java
+++ src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/ChangePinModelTest.java
@@ -6,31 +6,25 @@ import ch.bfh.btx8081.w2017.green.Spero.model.ChangePinModel;
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.IncorrectException;
 
 public class ChangePinModelTest {
-	
-	private ChangePinModel changePinModel;
 
-	@Test
-	public void testCheckOldPin() throws IncorrectException {
-		try {
-		changePinModel.checkOldPin("2345");
-		changePinModel.checkOldPin("1234");
-		}
-		catch (Exception e) {
-		      System.out.println(e);
-		}
+	@Test(expected = IncorrectException.class)
+	public void testCheckExceptions() throws IncorrectException {
+		ChangePinModel changePinModel1 = new ChangePinModel("1235","23","23");
+		changePinModel1.checkOldPin("1235");
+		changePinModel1.checkNewPin("23", "23");
+		ChangePinModel changePinModel2 = new ChangePinModel("", "", "");
+		changePinModel2.checkOldPin("");
+		changePinModel2.checkNewPin("", "");
+		ChangePinModel changePinModel3 = new ChangePinModel("abcd", "abcd", "abcd");
+		changePinModel3.checkOldPin("abcd");
+		changePinModel3.checkNewPin("abcd", "abcd");
 	}
 
 	@Test
-	public void testCheckNewPin() throws IncorrectException {
-		try {
-		changePinModel.checkNewPin("1", "1");
-		changePinModel.checkNewPin("", "");
-		changePinModel.checkNewPin("abcd", "abcd");
-		changePinModel.checkNewPin("1234", "1235");
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+	public void testCorrect() throws IncorrectException {
+		ChangePinModel changePinModel = new ChangePinModel("1234", "2345", "2345");
+		changePinModel.checkOldPin("1234");
+		changePinModel.checkNewPin("2345", "2345");
 	}
 
 }
