be8aa481f2becb600f401e91e1282cf23b808f52
mussi <mussi@147.87.243.11>
2018-01-12 09:38:53 +0100

    javadoc to Views class

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/view/Views.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/view/Views.java
@@ -1,72 +1,77 @@
 package ch.bfh.btx8081.w2017.green.Spero.view;
 
+/**
+ * This class includes all views which are able to navigate with the navigator class 
+ * and defines the URL shortcuts 
+ * 
+ * @author mussi
+ *
+ */
 public class Views {
-	// defines the URL shortcut FIXME now it has shortcut like
-		// bla.com/#!information
-		
-		/*
-		 * The main view
-		 */
-		public static final String MAIN_VIEW = "main";
-		
-		/*
-		 * The menu view
-		 */
-		public static final String MENU_VIEW = "menu";
-		
-		/*
-		 * The report view
-		 */
-		public static final String REPORT_VIEW = "report";
-		
-		/*
-		 * The settings view test
-		 */
-		public static final String SETTINGS_VIEW = "settings";
-		
-		/*
-		 * The sos view
-		 */
-		public static final String SOS_VIEW = "sos";
-		
-		/*
-		 * The chat view
-		 */
-		public static final String CHAT_VIEW = "chat";
-		
-		/*
-		 * The DiaryViewImpl
-		 */
-		public static final String DIARY_VIEW = "diary";
-		
-		/*
-		 * The PIN view
-		 */
-		public static final String PIN_VIEW = "";
-		
-		/*
-		 * The changePinView
-		 */
-		public static final String CHANGE_PIN_VIEW = "changePin";
-		
-		/*
-		 * The ReminderView
-		 */
-		public static final String REMINDER_VIEW = "reminder";
-		
-		/*
-		 * The ForgetPasswordView
-		 */
-		public static final String FORGET_PASSWORD_VIEW = "forgetPassword";
-		
-		/*
-		 * The DefDepressionView
-		 */
-		public static final String DEFINITION_DEPRESSION_VIEW = "defDepression";
-		
-		/*
-		 * The MedInfoView
-		 */
-		public static final String MEDICATION_INFORMATION_VIEW = "medInfoView";
-	}
+
+	/*
+	 * The main view
+	 */
+	public static final String MAIN_VIEW = "main";
+
+	/*
+	 * The menu view
+	 */
+	public static final String MENU_VIEW = "menu";
+
+	/*
+	 * The report view
+	 */
+	public static final String REPORT_VIEW = "report";
+
+	/*
+	 * The settings view test
+	 */
+	public static final String SETTINGS_VIEW = "settings";
+
+	/*
+	 * The sos view
+	 */
+	public static final String SOS_VIEW = "sos";
+
+	/*
+	 * The chat view
+	 */
+	public static final String CHAT_VIEW = "chat";
+
+	/*
+	 * The DiaryViewImpl
+	 */
+	public static final String DIARY_VIEW = "diary";
+
+	/*
+	 * The PIN view
+	 */
+	public static final String PIN_VIEW = "";
+
+	/*
+	 * The changePinView
+	 */
+	public static final String CHANGE_PIN_VIEW = "changePin";
+
+	/*
+	 * The ReminderView
+	 */
+	public static final String REMINDER_VIEW = "reminder";
+
+	/*
+	 * The ForgetPasswordView
+	 */
+	public static final String FORGET_PASSWORD_VIEW = "forgetPassword";
+
+	/*
+	 * The DefDepressionView
+	 */
+	public static final String DEFINITION_DEPRESSION_VIEW = "defDepression";
+
+	/*
+	 * The MedInfoView
+	 */
+	public static final String MEDICATION_INFORMATION_VIEW = "medInfoView";
+}
 
