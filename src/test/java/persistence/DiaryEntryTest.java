1753b068b1b873ae19ec82c7ecd190e7dc55011d
schmt5 <32765638+schmt5@users.noreply.github.com>
2018-01-12 11:07:14 +0100

    Changes in Comments

--- src/test/java/persistence/DiaryEntryTest.java
+++ src/test/java/persistence/DiaryEntryTest.java
@@ -4,66 +4,101 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.Mood;
+import ch.bfh.btx8081.w2017.green.Spero.persistence.DiaryEntry;
+
 public class DiaryEntryTest {
 
-	@Test
-	public void testDiaryEntry() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDiaryEntryStringStringMood() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetDiaryEntryNumber() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testDiaryEntry() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDiaryEntryStringStringMood() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetDiaryEntryNumber() {
+//		fail("Not yet implemented");
+//	}
 
 	@Test
 	public void testSetMoodparam() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getMoodparam() == null);
+		
+		diaryEntry.setMoodparam(Mood.gut);
+		assertTrue(diaryEntry.getMoodparam().equals(Mood.gut));
 	}
 
-	@Test
-	public void testSetTag() {
-		fail("Not yet implemented");
-	}
-
+//	@Test
+//	public void testSetTag() {
+//		fail("Not yet implemented");
+//	}
+//
 	@Test
 	public void testSetText() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getText() == null);
+		
+		diaryEntry.setText("MyText");
+		assertTrue(diaryEntry.getText().equals("MyText"));
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getTitle() == null);
+		
+		diaryEntry.setTitle("MyTitle");
+		assertTrue(diaryEntry.getTitle().equals("MyTitle"));
 	}
 
 	@Test
 	public void testGetDiaryEntryNumber() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getDiaryEntryNumber() == 0);
+				
 	}
 
 	@Test
 	public void testGetMoodparam() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getMoodparam() == null);
+		
+		DiaryEntry diaryEntry2 = new DiaryEntry("MyTitle", "MyText", Mood.gut);
+		assertTrue(diaryEntry2.getMoodparam().equals(Mood.gut));
+		assertFalse(diaryEntry2.getMoodparam().equals(Mood.schlecht));
 	}
-
-	@Test
-	public void testGetTag() {
-		fail("Not yet implemented");
-	}
-
+//
+//	@Test
+//	public void testGetTag() {
+//		fail("Not yet implemented");
+//	}
+//
 	@Test
 	public void testGetText() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getText() == null);
+		
+		DiaryEntry diaryEntry2 = new DiaryEntry("MyTitle", "MyText", Mood.gut);
+		assertTrue(diaryEntry2.getText().equals("MyText"));
+		assertFalse(diaryEntry2.getText().equals("WrongText"));
+		
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		DiaryEntry diaryEntry = new DiaryEntry();
+		assertTrue(diaryEntry.getTitle() == null);
+		
+		DiaryEntry diaryEntry2 = new DiaryEntry("MyTitle", "MyText", Mood.gut);
+		assertTrue(diaryEntry2.getTitle().equals("MyTitle"));
+		assertFalse(diaryEntry2.getTitle().equals("WrongText"));
+		
+		
 	}
 
 }
