574a9030e86b038e0f6ea999b6ad8baba5e2a7c1
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2018-01-17 16:56:24 +0100

    comments

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/interfaceEnum/IncorrectException.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/interfaceEnum/IncorrectException.java
@@ -1,7 +1,18 @@
 package ch.bfh.btx8081.w2017.green.Spero.interfaceEnum;
 
+/**
+ * the IncorrectException class
+ * @author mirjam
+ *
+ */
 public class IncorrectException extends Exception {
-	
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * throws an exception and shows a message, why the exception is thrown
+	 * @param message
+	 */
 	public IncorrectException(String message){
 		super(message);
 	}
--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/ForgetPasswordModel.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/model/ForgetPasswordModel.java
@@ -1,12 +1,17 @@
 package ch.bfh.btx8081.w2017.green.Spero.model;
 
 /**
- * checks the input mail address, if it is well formed
+ * checks the input mail address
  * @author mirjam
  *
  */
 public class ForgetPasswordModel {
 	
+	  	/**
+	  	 * checks the input mail address, if it is well formed
+	  	 * @param the mail address
+	  	 * @return true if it is well formed and false if the mail address isn't well formed
+	  	 */
 		public static boolean checkEmail(String mail){
 			
 			boolean correctEmail = true;
