ba5821c7bf89f6f70dd0ec376f1dd7125e24f482
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2018-01-17 15:43:13 +0100

    junit

--- src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/SettingsModelTest.java
+++ src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/SettingsModelTest.java
@@ -4,24 +4,35 @@ import org.junit.Test;
 
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.IncorrectException;
 
+/**
+ * tests the settingsModel class
+ * @author mirjam
+ *
+ */
 public class SettingsModelTest {
 	
+	/**
+	 * tests if an exception is thrown
+	 * @throws IncorrectException
+	 */
 	@Test (expected = IncorrectException.class)
 	public void testExceptions() throws IncorrectException {
 			SettingsModel settingsModel = new SettingsModel("username", "", "", "on");
 			settingsModel.checkSnooze("1234");
 			settingsModel.checkSnooze("");
 			settingsModel.checkSnooze("ea");
-			
 			settingsModel.checkReminder("");
 	}
 
+	/**
+	 * tests if no exception is thrown
+	 * @throws IncorrectException
+	 */
 	@Test
 	public void testCorrect() throws IncorrectException {
 			SettingsModel settingsModel = new SettingsModel("username", "10", "123", "on");
 			settingsModel.checkSnooze("123");
 			settingsModel.checkReminder("10");
-		
 	}
 
 }
