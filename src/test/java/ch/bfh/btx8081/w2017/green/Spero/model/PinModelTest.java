09c3dbea3b1a7f0142f02f224dae6d8fdb3ed030
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2018-01-17 15:42:54 +0100

    junit

--- src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/PinModelTest.java
+++ src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/PinModelTest.java
@@ -4,14 +4,27 @@ import org.junit.Test;
 
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.IncorrectException;
 
+/**
+ * tests the PinModel class with all the methods
+ * @author mirjam
+ *
+ */
 public class PinModelTest {
 
+	/**
+	 * tests if an exception is thrown
+	 * @throws IncorrectException
+	 */
 	@Test(expected = IncorrectException.class)
 	public void testPinModel() throws IncorrectException {
 		PinModel pinModel = new PinModel("1111");
 		pinModel.checkPin("1111");
 	}
 
+	/**
+	 * tests if no exception is thrown
+	 * @throws IncorrectException
+	 */
 	@Test
 	public void testCorrect() throws IncorrectException {
 		PinModel pinModel = new PinModel("1234");
