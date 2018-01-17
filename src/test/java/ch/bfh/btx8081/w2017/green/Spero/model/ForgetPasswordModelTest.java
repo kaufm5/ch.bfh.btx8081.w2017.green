84e202c325b8496483b351948f8648dac03c2b37
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2018-01-17 15:42:32 +0100

    junit

--- src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/ForgetPasswordModelTest.java
+++ src/test/java/ch/bfh/btx8081/w2017/green/Spero/model/ForgetPasswordModelTest.java
@@ -5,18 +5,24 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+/**
+ * tests the ForgetPasswordModel Class
+ * @author mirjam
+ *
+ */
 public class ForgetPasswordModelTest {
 
+	/**
+	 * tests if the email is correctly checked
+	 */
 	@Test
 	public void testCheckEmail() {
-		
 		assertFalse(ForgetPasswordModel.checkEmail("jaksdfgmailcom"));
 		assertFalse(ForgetPasswordModel.checkEmail("jaksdf@gmailcom"));
 		assertFalse(ForgetPasswordModel.checkEmail("@gmail.com"));
 		assertFalse(ForgetPasswordModel.checkEmail("jaksdf@gmail."));
 		assertFalse(ForgetPasswordModel.checkEmail("jaksdf@.com"));
 		assertTrue(ForgetPasswordModel.checkEmail("jaksdf@gmail.com"));
-			
 	}
 
 }
