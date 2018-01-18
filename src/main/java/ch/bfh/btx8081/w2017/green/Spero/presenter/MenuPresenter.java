21b02f91b831e374cc325a80bb27d00837d201f5
kaufm5 <28012549+kaufm5@users.noreply.github.com>
2018-01-18 20:00:09 +0100

    menu

--- src/main/java/ch/bfh/btx8081/w2017/green/Spero/presenter/MenuPresenter.java
+++ src/main/java/ch/bfh/btx8081/w2017/green/Spero/presenter/MenuPresenter.java
@@ -3,11 +3,13 @@ package ch.bfh.btx8081.w2017.green.Spero.presenter;
 import com.vaadin.ui.Button.ClickEvent;
 
 import ch.bfh.btx8081.w2017.green.Spero.interfaceEnum.SperoViewListener;
+import ch.bfh.btx8081.w2017.green.Spero.model.MenuModel;
 import ch.bfh.btx8081.w2017.green.Spero.view.MenuView;
 import ch.bfh.btx8081.w2017.green.Spero.view.Views;
 
 /**
- * 
+ * connects the view with the model
+ * handles the events
  * @author mirjam
  *
  */
@@ -15,34 +17,48 @@ public class MenuPresenter implements SperoViewListener {
 
 	private static final long serialVersionUID = 1L;
 	private MenuView menuView;
+	private MenuModel menuModel;
 	
-	public MenuPresenter(MenuView menuView) {
+	public MenuPresenter(MenuView menuView, MenuModel menuModel) {
 		this.menuView = menuView;
+		this.menuModel = menuModel;
 		menuView.init();
 		menuView.addListener(this);
 	}
-// test
+
+	
 	@Override
 	public void buttonClick(ClickEvent event) {
 		String buttonId = event.getButton().getId();
 		
 		switch(buttonId) {
+		
+		//opens the mainView if the user clicks the gefühlslage button
 		case "gefuehlslage":
 			menuView.getUI().getNavigator().navigateTo(Views.MAIN_VIEW);
 		    break;
+		    
+		//opens the diaryView if the user clicks the diary button    
 		case "diary":
 			menuView.getUI().getNavigator().navigateTo(Views.DIARY_VIEW);
 		    break;
+		    
+		//opens the reportView if the user clicks the report button    
 		case "report":
 			menuView.getUI().getNavigator().navigateTo(Views.REPORT_VIEW);
 			break;
+			
+		//opens the reminderView if the user clicks the reminder button	
 		case "reminder":
 			menuView.getUI().getNavigator().navigateTo(Views.REMINDER_VIEW);
 		    break;
+		    
+		//opens the medInfoView if the user clicks the medication information button    
 		case "medInfo":
-			//menuView.getUI().getPage().setLocation("https://compendium.ch/home/de");
 			menuView.getUI().getNavigator().navigateTo(Views.MEDICATION_INFORMATION_VIEW);
 		    break;
+		    
+		//opens the depErkView if the user clicks the definition depression button    
 		case "depErk":
 			menuView.getUI().getNavigator().navigateTo(Views.DEFINITION_DEPRESSION_VIEW);
 		    break;
